c9e41d9cf25aeeaaf010168ebf12342636566ccb
yosi475 <yosi475@naver.com>
2026-04-24 09:16:32 +0900

    Update 1-1.c

---
+++ code/1-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char s1[10] = "hi";
+    char s2[] = "hi";
+    char s3[] = {'h', 'i', '\0'};
+
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+    printf("%s\n", s3);
+
+    return 0;
+}
