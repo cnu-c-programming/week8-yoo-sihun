696f29c60cc9aaf9318eb55ab2943bf28aba4d6d
yosi475 <yosi475@naver.com>
2026-04-24 09:22:30 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void foo() {
+    char *str1 = "hello";
+    printf("%p\n", (void*)str1);
+}
+
+void bar() {
+    char *str2 = "hello";
+    printf("%p\n", (void*)str2);
+}
+
+int main() {
+    foo();
+    bar();
+    return 0;
+}
